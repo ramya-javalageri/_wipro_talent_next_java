2c62253743216032982490dadef2b4ecd4cff877
Ramya Javalageri <167241795+ramya-javalageri@users.noreply.github.com>
2025-09-03 13:44:23 +0530

    Add files via upload

---
+++ Basic/OnesComplementInput.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class OnesComplementInput {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter a number: ");
+int a = sc.nextInt();
+System.out.println("One's Complement: " + (~a));
+sc.close();
+}
+}
